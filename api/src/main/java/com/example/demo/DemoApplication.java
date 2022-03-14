854412299d905372db00598e070e0a4d67788394
TatsutoshiMaeda <tatsutoshi.maeda@banana-877.com>
2022-03-14 19:23:25 +0900

    バックエンド側コンテナ追加

---
+++ api/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@RequestMapping("/")
+  	public String home() {
+    	return "ホーム";
+  	}
+	
+	@RequestMapping("/login")
+  	public String login() {
+    	return "ログイン画面";
+  	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
