d4d77126c82d2893303d5db171318d1f8fccf820
TatsutoshiMaeda <tatsutoshi.maeda@banana-877.com>
2022-03-24 00:55:24 +0900

    SQLiteへ変更 引き続きDBコンテナへの接続不可の調査は継続する

--- app/src/main/java/com/example/demo/WebSecurityConfiguration.java
+++ app/src/main/java/com/example/demo/WebSecurityConfiguration.java
@@ -1,19 +1,46 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    
     @Override
     public void configure(HttpSecurity http) throws Exception {
        http.csrf().disable().authorizeRequests()
         .antMatchers("/").permitAll()
         .antMatchers(HttpMethod.POST, "/add/*").permitAll()
-        .antMatchers(HttpMethod.GET,"/master/*").permitAll()
         .antMatchers(HttpMethod.GET,"/start").permitAll()
-        .anyRequest().authenticated();
+        .antMatchers("/", "/home").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
     }
+
+    @Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
 }
--- app/src/main/java/com/example/demo/controller/SampleController.java
+++ app/src/main/java/com/example/demo/controller/SampleController.java
@@ -2,9 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -28,6 +32,21 @@ public class SampleController {
   // 指定した name のデータを追加する
   @RequestMapping("/start")
   public String aaaString() {
+    service.findAll();
     return "起動確認";
   }
+
+  @GetMapping("/signup")
+    public String getSignUp(Model model) {
+        // signup.htmlに画面遷移
+        return "login/signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSignUp(Model model) {
+
+        // login.htmlにリダイレクト
+        return "redirect:/login";
+    }
+
 }
