0185960b1cb930ab05f10e10eefd89b1a1708e39
TatsutoshiMaeda <tatsutoshi.maeda@banana-877.com>
2022-03-28 19:24:09 +0900

    docker関連の不要ファイル削除

--- docker/database/init/1_ddl.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user (
-  id   int NOT NULL AUTO_INCREMENT,
-  name varchar(255) not null,
-  password varchar(255),
-  PRIMARY KEY (id)
-);
--- docker/database/init/2_insert.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user (name, password) 
-VALUES('demo','demo');
