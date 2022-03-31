3b473bab8697eb4ba626cb28c4d2816b4046e083
TatsutoshiMaeda <tatsutoshi.maeda@banana-877.com>
2022-03-31 18:12:41 +0900

    DDLの追加（flyway管理）

---
+++ app/src/main/resources/db/migration/V1_0_0__Create_user_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user(
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name text,
+    email text,
+    password text
+);
---
+++ app/src/main/resources/db/migration/V1_0_1__Insert_user_column.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (user_name, email, password) VALUES
+(
+    'user',
+    'demo@test.com',
+    'password'
+),
+(
+    'pompom',
+    'pom@pom.purin',
+    'purin'
+); 
